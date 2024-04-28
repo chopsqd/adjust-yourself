17263ecd0ff8e37bbb8fa2e8c4c5158c27154afa
Michael Chop <007micle007@gmail.com>
2024-04-28 18:10:05 +0300

    Added the console icon to the AppButton component

--- ui-kit/AppButton/AppButton.utils.tsx
+++ ui-kit/AppButton/AppButton.utils.tsx
@@ -180,6 +180,24 @@ const edit = (
    </Svg>
 )
 
+const console = (
+   <Svg
+      width={26}
+      height={26}
+      viewBox={"0 0 16 16"}
+      style={{ margin: 7 }}
+   >
+      <Path
+         d={"M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"}
+         fill={"#fff"}
+      />
+      <Path
+         d={"M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"}
+         fill={"#fff"}
+      />
+   </Svg>
+)
+
 export const getContent = (title: string, fontSize?: FontSizeEnum): React.JSX.Element => {
    switch (title) {
       case "gear":
@@ -200,6 +218,8 @@ export const getContent = (title: string, fontSize?: FontSizeEnum): React.JSX.El
          return compile
       case "edit":
          return edit
+      case "console":
+         return console
       default:
          return (
             <Text
